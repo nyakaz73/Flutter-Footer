9ae72dea662e17cb25ba712d70ee31bf426d3e1c
Tafadzwa Nyamukapa <tafadzwalnyamukapa@gmail.com>
2020-08-02 13:05:36 +0200

    Dart code formating

--- lib/footer_view.dart
+++ lib/footer_view.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:footer/footer.dart';
-class FooterView extends StatefulWidget{
+
+class FooterView extends StatefulWidget {
   final List<Widget> children;
   final Footer footer;
   final int flex;
-  FooterView({@required this.children, @required this.footer,this.flex}){
-    if(flex != null) {
+  FooterView({@required this.children, @required this.footer, this.flex}) {
+    if (flex != null) {
       if (this.flex > 10 || this.flex < 1) {
         throw ArgumentError('Only 1-10 Flex range is allowed');
       }
@@ -16,37 +17,34 @@ class FooterView extends StatefulWidget{
     return new FooterViewState();
   }
 }
-class FooterViewState extends State<FooterView>{
+
+class FooterViewState extends State<FooterView> {
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: <Widget>[
-        SliverPadding(
-          padding: const EdgeInsets.all(0.0),
-          sliver: new SliverList(
-            delegate: new SliverChildListDelegate(
-              widget.children, //All children inside Goes here including Columns , Containers, Expanded, ListViews
+    return CustomScrollView(slivers: <Widget>[
+      SliverPadding(
+        padding: const EdgeInsets.all(0.0),
+        sliver: new SliverList(
+          delegate: new SliverChildListDelegate(
+            widget
+                .children, //All children inside Goes here including Columns , Containers, Expanded, ListViews
+          ),
+        ),
+      ),
+      SliverFillRemaining(
+        hasScrollBody: false,
+        child: new Column(
+          children: <Widget>[
+            new Expanded(
+              flex: widget.flex == null ? 8 : 10 - widget.flex,
+              child: new Container(),
             ),
-          ),
+            new Expanded(
+                flex: widget.flex == null ? 2 : widget.flex,
+                child: widget.footer)
+          ],
         ),
-        SliverFillRemaining(
-          hasScrollBody: false,
-          child: new Column(
-            children: <Widget>[
-              new Expanded(
-                flex: widget.flex == null ?8 : 10-widget.flex,
-                  child: new Container(),
-              ),
-              new Expanded(
-                flex: widget.flex == null? 2: widget.flex,
-                  child: widget.footer
-              )
-            ],
-          ),
-        ),
-      ]
-
-    );
+      ),
+    ]);
   }
-
 }
