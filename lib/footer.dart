5765139e0ae17fddb87a76e7b059f46c75ceacb2
Tafadzwa Nyamukapa <tafadzwalnyamukapa@gmail.com>
2020-08-02 13:05:09 +0200

    Changed Footer padding attribute from double to EdgeInsetsGeometry

--- lib/footer.dart
+++ lib/footer.dart
@@ -3,30 +3,40 @@ library footer;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Footer extends StatefulWidget{
+class Footer extends StatefulWidget {
   //final int flex;
   final Color backgroundColor;
   final Alignment alignment;
-  final double padding;
+  //final double padding;
+  final EdgeInsetsGeometry padding;
   final Widget child;
-  Footer({this.backgroundColor,this.alignment,this.padding, @required this.child});
+  Footer(
+      {this.backgroundColor,
+      this.alignment,
+      this.padding,
+      @required this.child});
   @override
-  State createState()=> FooterState();
+  State createState() => FooterState();
 }
 
-class FooterState extends State<Footer>{
+class FooterState extends State<Footer> {
   @override
   Widget build(BuildContext context) {
     return new Container(
-      color: widget.backgroundColor ==null?Colors.grey.shade200:widget.backgroundColor,
+      color: widget.backgroundColor == null
+          ? Colors.grey.shade200
+          : widget.backgroundColor,
       child: new Align(
-        alignment: widget.alignment == null?Alignment.bottomCenter:widget.alignment,
+        alignment: widget.alignment == null
+            ? Alignment.bottomCenter
+            : widget.alignment,
         child: new Padding(
-            padding: EdgeInsets.all(widget.padding == null ?5.0:widget.padding),
+          //padding: EdgeInsets.all(widget.padding == null ?5.0:widget.padding),
+          padding:
+              widget.padding == null ? EdgeInsets.all(5.0) : widget.padding,
           child: widget.child,
         ),
       ),
     );
   }
-
 }
